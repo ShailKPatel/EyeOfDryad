e697d3d33d6399d8ae4f5da763f909c485a6c508
Shail K Patel <shailpatel.connect@gmail.com>
2025-07-12 15:14:53 +0530

    updated requirments.txt, corrected routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,7 @@
 from flask import Blueprint, render_template
-from .forms import UserForm
-from .models import User, db
 
 main = Blueprint('main', __name__)
 
-@main.route('/', methods=['GET', 'POST'])
+@main.route("/")
 def home():
-    return render_template("home.html", form=UserForm(), users=User.query.all())
+    return render_template("home.html")
